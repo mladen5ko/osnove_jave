926bf8ca8e070f645391e25459c2485eb65b673b
Mladen Petkovic <mladenpetkovic991@gmail.com>
2022-05-13 20:36:07 +0200

    Predavanja 13.05 Git Zadatak 1

---
+++ osnove_jave/src/main/java/p13_05_2022/Git_Zadatak1.java
@@ -0,0 +1,12 @@
+package p13_05_2022;
+
+public class Git_Zadatak1 {
+    public static void main(String[] args) {
+        System.out.println("git clone");
+        System.out.println("git status");
+        System.out.println("git add");
+        System.out.println("git commit -m");
+        System.out.println("git push");
+        System.out.println("touch");
+    }
+}

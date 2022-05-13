a3fd750ff47f1f475a7938eaba828bf3c806408b
Mladen Petkovic <mladenpetkovic991@gmail.com>
2022-05-13 19:59:33 +0200

    Minor typo fix

--- osnove_jave/src/main/java/p13_05_2022/Zadatak1.java
+++ osnove_jave/src/main/java/p13_05_2022/Zadatak1.java
@@ -2,6 +2,6 @@ package p13_05_2022;
 
 public class Zadatak1 {
     public static void main(String[] args) {
-        System.out.println("Hello borld");
+        System.out.println("Hello World");
     }
 }
